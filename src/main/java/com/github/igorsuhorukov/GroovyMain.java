bf2f71826de715f7ba0d026496a8a9876134606b
igor-suhorukov <igor.suhorukov@gmail.com>
2016-02-03 00:26:11 +0300

    Remove quickfix, update classloader to next dev version. Append maven url factory to resolve URL like mvn:/group:artifact:version[?REPO_URL]

--- src/main/java/com/github/igorsuhorukov/GroovyMain.java
+++ src/main/java/com/github/igorsuhorukov/GroovyMain.java
@@ -1,8 +1,11 @@
 package com.github.igorsuhorukov;
 
+import java.net.URL;
+
 public class GroovyMain {
 
     public static void main(String[] args) throws Exception{
+        URL.setURLStreamHandlerFactory(new MavenURLStreamHandlerFactory());
         GroovyClassLoaderFactory.createGroovyClassLoader();
         groovy.ui.GroovyMain.main(args);
     }
--- src/main/java/com/github/igorsuhorukov/springframework/util/Assert.java
+++
@@ -1,9 +0,0 @@
-package com.github.igorsuhorukov.springframework.util;
-
-public class Assert {
-    public static void notNull(Object object, String message) {
-        if (object == null) {
-            throw new IllegalArgumentException(message);
-        }
-    }
-}
