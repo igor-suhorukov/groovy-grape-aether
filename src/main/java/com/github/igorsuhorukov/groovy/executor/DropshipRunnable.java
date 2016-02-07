80ce99e1a35ce3dc585118d14bc4d8f9192b650e
igor-suhorukov <igor.suhorukov@gmail.com>
2016-02-07 23:20:12 +0300

    allow user execute Dropship main method by using ExecutorService

---
+++ src/main/java/com/github/igorsuhorukov/groovy/executor/DropshipRunnable.java
@@ -0,0 +1,43 @@
+package com.github.igorsuhorukov.groovy.executor;
+
+import com.github.igorsuhorukov.smreed.dropship.Dropship;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import static com.github.igorsuhorukov.google.common.base.Ascii.checkArgument;
+import static com.github.igorsuhorukov.google.common.base.Ascii.checkNotNull;
+
+public class DropshipRunnable implements Runnable, Serializable{
+
+    private String[] args;
+    private Map<String, String> systemProperties;
+
+    public DropshipRunnable(String... args) {
+        args = checkNotNull(args);
+        checkArgument(args.length >= 2, "Must specify groupId:artifactId[:version] and classname!");
+        this.args = args;
+    }
+
+    public DropshipRunnable(Map<String, String> systemProperties, String... args) {
+        this(args);
+        this.systemProperties = systemProperties;
+    }
+
+    public void run() {
+        setSystemProperties();
+        try {
+            Dropship.main(args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setSystemProperties() {
+        if(systemProperties!=null && !systemProperties.isEmpty()) {
+            for(Map.Entry<String, String> property: systemProperties.entrySet()){
+                System.setProperty(property.getKey(), property.getValue());
+            }
+        }
+    }
+}
